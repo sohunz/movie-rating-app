2b4a3fb4517a084a410812599453ed5206eaa539
sohunz <sohunily@gmail.com>
2024-05-29 20:11:50 +0700

    fix ui and replace logout btn

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
+    const handleLogout = () => {
+        localStorage.removeItem("guest_session_id");
+        location.reload();
+    };
+
     return (
-        <div className="w-full border mb-5">
+        <div className="w-[90vw] border mb-5">
             <div className="flex items-center justify-between">
                 <div className="flex items-center">
                     <Link
@@ -19,12 +32,26 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div>
-                    <Link
-                        to="/auth"
-                        className="py-3 px-5 cursor-pointer hover:bg-gray-300"
-                    >
-                        Auth
-                    </Link>
+                    {localStorage.getItem("guest_session_id") ? (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                {localStorage.getItem("guest_session_id")
+                                    ? "Guest"
+                                    : "Login"}
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>
+                                    Account Setting
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={handleLogout}>
+                                    Logout
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    ) : (
+                        "Login"
+                    )}
                 </div>
             </div>
         </div>
--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,4 +1,4 @@
-import { LogOut, Mail } from "lucide-react";
+import { Mail } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useMutation } from "@tanstack/react-query";
 import { mutationLogin } from "./mutationLogin";
@@ -13,7 +13,8 @@ const Auth = () => {
         onSuccess: (data) => {
             if (data && data.guest_session_id) {
                 localStorage.setItem("guest_session_id", data.guest_session_id);
-                navigate("/movie");
+                navigate("/");
+                location.reload();
             } else {
                 console.error(
                     "guest_session_id is missing in the response data"
@@ -29,23 +30,12 @@ const Auth = () => {
         mutate();
     };
 
-    const handleLogout = () => {
-        localStorage.removeItem("guest_session_id");
-        navigate("/movie");
-    };
-
     return (
         <div className="w-full h-[85vh] flex items-center justify-center">
-            {localStorage.getItem("guest_session_id") ? (
-                <Button className="border" onClick={handleLogout}>
-                    <LogOut className="mr-2 h-4 w-4" /> Logout
-                </Button>
-            ) : (
-                <Button className="border" onClick={handleLogin}>
-                    <Mail className="mr-2 h-4 w-4" /> Login by registering as a
-                    Guest
-                </Button>
-            )}
+            <Button className="border" onClick={handleLogin}>
+                <Mail className="mr-2 h-4 w-4" /> Login by registering as a
+                Guest
+            </Button>
         </div>
     );
 };
