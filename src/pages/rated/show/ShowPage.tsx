f40f32466502167b0770a3842dd6de6e41056f2b
sohunz <sohunily@gmail.com>
2024-06-01 22:15:54 +0700

    UI done

--- src/pages/rated/show/ShowPage.tsx
+++ src/pages/rated/show/ShowPage.tsx
@@ -9,7 +9,7 @@ const ShowPage = () => {
         return (
             <div className="mb-8 mt-10">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {Array.from({ length: 12 }).map((_, index) => (
+                    {Array.from({ length: 4 }).map((_, index) => (
                         <CardSketon key={index} />
                     ))}
                 </div>
